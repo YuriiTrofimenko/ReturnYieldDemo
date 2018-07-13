cec816957716e01b127b3330ff76118c4abd7934
Yurii Trofimenko <tyaa@ukr.net>
2018-07-13 16:56:59 +0300

    init

--- ReturnYieldDemo/Program.cs
+++ ReturnYieldDemo/Program.cs
@@ -10,6 +10,19 @@ namespace ReturnYieldDemo
     {
         static void Main(string[] args)
         {
+            var day = GetDays();
+            //Console.WriteLine(day.Take(1).SingleOrDefault());
+            //Console.WriteLine(day.Skip(1).Take(1).SingleOrDefault());
+            foreach (var item in day)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static IEnumerable<string> GetDays() {
+
+            yield return "Понедельник";
+            yield return "Вторник";
         }
     }
 }
